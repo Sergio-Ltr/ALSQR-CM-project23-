61651dcd6fc22ea5c81e05363a7ab9fd3733262d
Sergio <Sergio.ltr96@gmail.com>
2022-11-14 15:16:12 +0100

    V_0 Init

--- algorithm/Initialization/Initialize_V.m
+++ algorithm/Initialization/Initialize_V.m
@@ -13,7 +13,7 @@
 %   sample values of V from a distributuio specifying its parameters according to
 %   RANDRAW" module: https://it.mathworks.com/matlabcentral/fileexchange/7309-randraw
 % - "sparse": 
-%    A percentage of the elements of V_0 will be 0s, according the d parameter (0 < d < 1). 
+%    A percentage of the elements of V_0 will be 0s, by default half, or according the d parameter (0 < d < 1) if specified correctly. 
 % - "orth":
 %   The returned n x k matrix will be composed by k orthonormal columns.
 % - "low_rank": 
@@ -26,8 +26,8 @@
 % Random matrix composed by 12 orthonormal columns (of 30 elements each). 
 % V = initialize_V(30, 12, "orth");
 %
-% Random matrix with 50% sparsity (Half of the elements being 0s). 
-% V = initialize_V(30, 12, "sparse", 0.5);
+% Random matrix with 40% sparsity (Half of the elements being 0s). 
+% V = initialize_V(30, 12, "sparse", 0.6);
 %
 % Matrix with shape 30 x 12 sampled from a Gaussian distribution with mean
 % 0 and stdev=1. 
@@ -38,20 +38,26 @@ function [V] = Initialize_V (n, k, constraint_key, param)
     
 mat = randn(n,k);
 
-%Initialization of the V_0 matrix. 
+%Constraint application the V_0 matrix. 
+
 if nargin > 2
     if constraint_key == "dist" %second item is distribution type (i.e. "normal"). Possibilities here: 
-            
+        throw(MException("Not implemented error", "Sorry, this function is not implemented yet"))    
         %dist = randraw(); % = Vo (starting point) 
     elseif  constraint_key == "sparse" %second item is density coefficient 0 < d < 1.  
-            
-        mat = sprandn(n,k, d); % = Vo (starting point) 
+        d = 0.5;
+        if param > 0 && param < 1
+            d = param;
+        end
+        mat = sprandn(n, k, d); % = Vo (starting point) 
     elseif  constraint_key == "orth"
-            
         mat = orth(mat); % output a matrix of orthogonal rows 
     elseif  constraint_key == "low_rank"
-            
-        mat = randn(n,k); % = Vo (starting point) 
+        r = k - 1;
+        if param > 1 && param < k
+            r = param;
+        end
+        mat = randn(n,r)*rand(r,k); % resulting matrix will have rank r by construction.  
     end  
 end
 
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-Resulsts at: https://drive.google.com/drive/folders/10mFGBTKXmi-9MzdD4eTtgZ4XGMwh0z_N?usp=share_link
+To open the project use Matplab "Open" button and select the LowRankWithQR.prj file in this folder. 
+
+Command list: 
+
+[U,V] = Solver(A, k) Use the low rank approximator to factorize matrix A of shape m x n into two matrices U m x k and V k x n. 
+
+
+Report: https://it.overleaf.com/project/625fc293c3d7b55c35517bbc
+Resulsts: https://drive.google.com/drive/folders/10mFGBTKXmi-9MzdD4eTtgZ4XGMwh0z_N?usp=share_link
